8cef3972fc63ac3cedbe819dcf3fd027eddcc2dc
Allan08 <allan.felizzeti@ksys.com.br>
2022-11-17 14:14:17 -0300

    Ultimas atualizações do projeto

--- ignite/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ ignite/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,16 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/AllanFelizzeti.png" />
+            <Avatar hasBorder={false} src="https://github.com/rheneaaa.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                 <header>
                     <div className={styles.authorAndTime}>
-                        <strong>Allan V Felizzeti</strong>
+                        <strong>Rhene Araujo</strong>
 
                         <time title="11 de maio ás 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                     </div>
@@ -20,7 +21,7 @@ export function Comment() {
                     </button>
                 </header>
 
-                <p>Muito bom Devon, Parabéns!! 👏👏</p>
+                <p>Muito bom Bezalel, Parabéns!! 👏👏</p>
                 </div>
             <footer>
                 <button>
