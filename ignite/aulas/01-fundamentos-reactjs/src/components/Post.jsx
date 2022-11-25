26533ccc80352bb9a066c1866be3915d96bdad56
ALLAN VERGILIO FELIZZETI <alllanvergilio@gmail.com>
2022-11-25 07:45:59 -0300

    Função de Like Finalizada

--- ignite/aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -30,7 +30,12 @@ export function Post({author, publishedAT, content}) {
     }
 
     function hendleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse camnpo é obrigatório!');
     }
 
     function deleteComment(commentToDelete) {
@@ -40,6 +45,8 @@ export function Post({author, publishedAT, content}) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return (
         <article  className={styles.post}>
@@ -72,10 +79,12 @@ export function Post({author, publishedAT, content}) {
                     placeholder='Deixe seu Comentário' 
                     value={newCommentText}
                     onChange={hendleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
